0f909b6fbaffe902dc69660ca7d358c79929f990
OnyAndewi <ony@undiksha.ac.id>
2021-10-08 19:59:08 +0800

    mengganti kode

--- resources/views/layouts/user2-template.blade.php
+++ resources/views/layouts/user2-template.blade.php
@@ -174,7 +174,7 @@
                     <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=9">
                 </div>
                 <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-6">
-                    <i class="fab fa-instagram mr-2"></i> Follow @dgrzyb
+                    <i class="fab fa-instagram mr-2"></i> Follow @sewarumahkostbali
                 </a>
             </div>
 
